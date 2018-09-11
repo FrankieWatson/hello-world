3c6ec39b134d2acd8ed989fa3326c36ff61675d8
Paul <paul.farah@publicboard.ca>
2018-09-10 20:00:28 -0400

    Done

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 echo 'hello, world';
+echo '<br>';
 echo 'another1';
-=======
-echo 'XXX_hello, world';
-echo 'another';
->>>>>>> 85d05067fb9a740049103c68dfc10cd30666f302
+
 
 ?>
